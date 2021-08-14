0920e1bc68223879121d99b8e5bb4d4ec308038f
Romain Leroux <romain@leroux.dev>
2021-08-14 23:40:13 +0200

    Add monsters

--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,17 @@ impl State {
     fn new() -> Self {
         let mut ecs = World::default();
         let mut resources = Resources::default();
-
         let mut rng = RandomNumberGenerator::new();
+
         let map_builder = MapBuilder::new(&mut rng);
 
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
