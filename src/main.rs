466d86a169a5b9cc7abfcae2402e01bc38494f17
Romain Leroux <romain@leroux.dev>
2021-08-15 22:53:10 +0200

    Add hud and tooltips (from mouse position)

--- src/main.rs
+++ src/main.rs
@@ -74,8 +74,17 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
 
-        self.resources.insert(ctx.key); // keyboard state
+        ctx.set_active_console(2);
+        ctx.cls();
 
+        // keyboard state
+        self.resources.insert(ctx.key);
+
+        // mouse position
+        ctx.set_active_console(0);
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
+
+        // turn transitions
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
         match current_state {
             TurnState::AwaitingInput => self
@@ -102,8 +111,10 @@ fn main() -> BError {
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
