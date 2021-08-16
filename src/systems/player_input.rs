9d31fbb8a152d53d05e5eb6a88ae31f6b2755e13
Romain Leroux <romain@leroux.dev>
2021-08-16 22:49:43 +0200

    Add healing system

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -14,6 +16,7 @@ pub fn player_input(
         Some(VirtualKeyCode::Right) => Point::new(1, 0),
         Some(VirtualKeyCode::Up) => Point::new(0, -1),
         Some(VirtualKeyCode::Down) => Point::new(0, 1),
+        Some(_) => Point::new(0, 0),
         _ => return,
     };
 
@@ -23,25 +26,38 @@ pub fn player_input(
         .find_map(|(&entity, &pos)| Some((entity, pos + delta)))
         .unwrap();
 
-    let mut hit_something = false;
-    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
-    enemies
-        .iter(ecs)
-        .filter(|(_, &pos)| pos == destination)
-        .for_each(|(&entity, _)| {
-            hit_something = true;
+    // No movement => heal
+    if delta == Point::new(0, 0) {
+        ecs.entry_mut(player_entity)
+            .unwrap()
+            .get_component_mut::<Health>()
+            .map(|mut health| {
+                health.current = i32::min(health.max, health.current + 1);
+            })
+            .ok();
+    } else
+    // Move or attack
+    {
+        let mut hit_something = false;
+        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        enemies
+            .iter(ecs)
+            .filter(|(_, &pos)| pos == destination)
+            .for_each(|(&entity, _)| {
+                hit_something = true;
 
-            commands.push((WantsToAttack {
-                attacker: player_entity,
-                victim: entity,
+                commands.push((WantsToAttack {
+                    attacker: player_entity,
+                    victim: entity,
+                },));
+            });
+
+        if !hit_something {
+            commands.push((WantsToMove {
+                entity: player_entity,
+                destination,
             },));
-        });
-
-    if !hit_something {
-        commands.push((WantsToMove {
-            entity: player_entity,
-            destination,
-        },));
+        }
     }
 
     *turn_state = TurnState::PlayerTurn;
