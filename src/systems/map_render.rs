d2fe40bd453d4516896b41d8949cfebcac3c6a9e
Romain Leroux <romain@leroux.dev>
2021-08-20 19:41:07 +0200

    Add spatial memory for player fov, with tinted rendering

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -15,14 +15,22 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
     for y in camera.top_y..=camera.bottom_y {
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
+            let idx = map_idx(x, y);
 
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(x, y);
+            if map.in_bounds(pt)
+                && (player_fov.visible_tiles.contains(&pt) || map.revealed_tiles[idx])
+            {
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    WHITE
+                } else {
+                    DARK_GRAY
+                };
+
                 let glyph = match map.tiles[idx] {
                     TileType::Floor => to_cp437('.'),
                     TileType::Wall => to_cp437('#'),
                 };
-                draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), glyph);
+                draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), glyph);
             }
         }
     }
