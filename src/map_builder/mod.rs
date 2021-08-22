d424b8b4572826051e8bb30eee47bf04808a7e1f
Romain Leroux <romain@leroux.dev>
2021-08-22 15:09:54 +0200

    Add a cellular automata based implementation of MapArchitect

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,3 +1,4 @@
+mod cellular;
 mod empty;
 mod rooms;
 
@@ -17,9 +18,10 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     const NUM_ROOMS: usize = 20;
+    const NUM_MONSTERS: usize = 50;
 
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = rooms::RoomsArchitect;
+        let mut architect = cellular::CellularAutomataArchitect;
         architect.new(rng)
     }
 
@@ -112,5 +114,28 @@ impl MapBuilder {
                 self.apply_horizontal_tunnel(prev.x, new.x, new.y);
             }
         }
+    }
+
+    fn spawn_monsters(&self, start: &Point, rng: &mut RandomNumberGenerator) -> Vec<Point> {
+        let mut spawnable_tiles: Vec<Point> = self
+            .map
+            .tiles
+            .iter()
+            .enumerate()
+            .filter(|&(idx, &t)| {
+                t == TileType::Floor
+                    && DistanceAlg::Pythagoras.distance2d(*start, self.map.index_to_point2d(idx))
+                        > 10.0
+            })
+            .map(|(idx, _)| self.map.index_to_point2d(idx))
+            .collect();
+
+        let mut spawns = Vec::new();
+        for _ in 0..MapBuilder::NUM_MONSTERS {
+            let target_index = rng.random_slice_index(&spawnable_tiles).unwrap();
+            spawns.push(spawnable_tiles.remove(target_index));
+        }
+
+        spawns
     }
 }
